8e9989003d1bdf64c10e5a9fedf361fe10828a42
Josh Coward <jrc0114@auburn.edu>
2020-08-02 19:13:48 -0500

    Add files via upload

--- Java/IntegerLists.java
+++ Java/IntegerLists.java
@@ -4,7 +4,18 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class IntegerLists {
+	/*
+	 * I didn't notice any substantial difference between the two solution methods
+	 */
 	public static void main(String[] args) {
+		//usingDeque();
+		usingArray();
+	}
+
+	/**
+	 * A solution to this problem using a deque two pop off values from each end of the list
+	 */
+	public static void usingDeque() {
 		Scanner in = new Scanner(System.in);
 
 		int tests = in.nextInt();
@@ -31,11 +42,10 @@ public class IntegerLists {
 						break;
 					}
 
-					if (left) {
+					if (left)
 						list.pollFirst();
-					} else {
+					else
 						list.pollLast();
-					}
 				}
 			}
 
@@ -51,6 +61,7 @@ public class IntegerLists {
 				else
 					it = list.descendingIterator();
 
+				// StringBuilder is used to save on print time
 				StringBuilder sb = new StringBuilder();
 				
 				sb.append("[");
@@ -62,6 +73,78 @@ public class IntegerLists {
 				
 				sb.delete(sb.length() - 1, sb.length());
 				sb.append("]");
+
+				System.out.println(sb);
+			}
+		}
+	}
+
+	/**
+	 * A solution to this problem using an array
+	 * Two bounding integers are used in place of the two ends of a deque
+	 * These integers set the lower (inclusive) and upper (exclusive) bounds
+	 */
+	public static void usingArray() {
+		Scanner in = new Scanner(System.in);
+
+		int tests = in.nextInt();
+
+		for ( ; tests > 0; tests--) {
+			String cmds = in.next();
+			int listSize = in.nextInt();
+			String line = in.next();
+
+			String[] list = line.substring(1, line.length() - 1).split(",");
+			if (list[0].equals(""))
+				list = new String[0];
+
+			boolean left = true;
+			boolean error = false;
+			int l = 0, r = listSize;
+
+			for (int i = 0; i < cmds.length(); i++) {
+				char cmd = cmds.charAt(i);
+
+				if (cmd == 'R')
+					left = !left;
+				else {
+					if (l == r) {
+						error = true;
+						break;
+					}
+
+					if (left)
+						l++;
+					else
+						r--;
+				}
+			}
+
+			if (error)
+				System.out.println("error");
+			else if (l == r)
+				System.out.println("[]");
+			else {
+				// StringBuilder is used to save on print time
+				StringBuilder sb = new StringBuilder();
+
+				if (left) {
+					sb.append("[" + list[l]);
+
+					for (int i = l + 1; i < r; i++) {
+						sb.append("," + list[i]);
+					}
+
+					sb.append("]");
+				} else {
+					sb.append("[" + list[r - 1]);
+
+					for (int i = r - 2; i >= l; i--) {
+						sb.append("," + list[i]);
+					}
+
+					sb.append("]");
+				}
 
 				System.out.println(sb);
 			}
