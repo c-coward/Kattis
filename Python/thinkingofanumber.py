d75d816b43b5b77297d2d4f80e1debe760796f34
Josh Coward <jrc0114@auburn.edu>
2020-10-13 22:32:54 -0500

    Add files via upload

---
+++ Python/thinkingofanumber.py
@@ -0,0 +1,36 @@
+def main():
+	n = int(input())
+
+	while n:
+		lower = 0
+		upper = float("inf")
+		divs = []
+
+		for _ in range(n):
+			rule, helper, num = input().split()
+			num = int(num)
+
+			if rule == "greater":
+				lower = max(lower, num)
+			elif rule == "less":
+				upper = min(upper, num)
+			elif num != 1:
+				divs.append(num)
+
+		n = int(input())
+		
+		if upper == float("inf"):
+			print("infinite")
+			continue
+
+		divs.sort(reverse = True)
+		vals = list(range(lower + 1, upper))
+		for div in divs:
+			vals = [v for v in vals if not (v % div)]
+
+		if vals:
+			print(" ".join(map(str, vals)))
+		else:
+			print("none")
+
+main()
